34bcdae53c27c28aad4a40a5a353cc979f7b20e9
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-03-06 17:40:08 +0400

    new: file CurlService.php for send request to Telegram Bot API

---
+++ modules/service/CurlService.php
@@ -0,0 +1,34 @@
+<?php
+class CurlService
+{
+    public array $preparedData;
+    private string $telegramApiLink;
+
+    public function __construct(array $preparedData, string $telegramApiLink)
+    {
+        $this->preparedData = $preparedData;
+        $this->telegramApiLink = $telegramApiLink;
+    }
+
+    public function send()
+    {
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $this->telegramApiLink);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($this->preparedData));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_exec($ch);
+
+            if (curl_error($ch)) {
+                error_log(curl_error($ch) . " - " . curl_errno($ch));
+                return false; 
+            }
+
+            return true;
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+}
