3168d2dad4db03686c92c8b88ddc10c498838cfa
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-03-06 17:46:19 +0400

    update: added requires which include CurlService, UserService and config. Added get RAW input and defines USER_ID and CHAT_ID

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+if (!file_exists("config/config.php")) {
+    error_log("File config/config.php dont exist");
+    exit;
+}
+
+if (!file_exists("modules/service/CurlService.php")) {
+    error_log("File modules/service/CurlService.php dont exist");
+    exit;
+}
+
+if (!file_exists("modules/user/UserService.php")) {
+    error_log("File modules/user/UserService.php dont exist");
+    exit;
+}
+
+require "config/config.php";
+require "modules/service/CurlService.php";
+require "modules/user/UserService.php";
+
+$update = json_decode(file_get_contents('php://input'), true);
+$json_response = json_encode($update, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+if (!isset($update['message']['from']['id']) || !isset($update['message']['chat']['id'])) {
+    error_log("Ошибка: нет нужных полей. " . $update);
+    exit;
+}
+
+define("USER_ID", $update['message']['from']['id']);
+define("CHAT_ID", $update['message']['chat']['id']);
+
+file_put_contents("log.log", $json_response);
